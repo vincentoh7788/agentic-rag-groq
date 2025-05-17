611e7698b4537365dd606c8dea20881337d4bf1b
Vincent <ohshichew@hotmail.com>
2025-05-17 17:02:26 +0800

    Modify requirements

--- rag.py
+++ rag.py
@@ -1,3 +1,6 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
